8174f837cf35d60cb6ba7c199402395d8abcb237
Adrian M2 <amac2008@gmail.com>
2026-03-04 16:50:36 -0800

    python ingestion scripts

---
+++ 01-docker-terraform/ingest_data.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import click
+import pandas as pd
+from sqlalchemy import create_engine
+from tqdm import tqdm
+
+dtype = {
+    "VendorID": "Int64",
+    "passenger_count": "Int64",
+    "trip_distance": "float64",
+    "RatecodeID": "Int64",
+    "store_and_fwd_flag": "string",
+    "PULocationID": "Int64",
+    "DOLocationID": "Int64",
+    "payment_type": "Int64",
+    "fare_amount": "float64",
+    "extra": "float64",
+    "mta_tax": "float64",
+    "tip_amount": "float64",
+    "tolls_amount": "float64",
+    "improvement_surcharge": "float64",
+    "total_amount": "float64",
+    "congestion_surcharge": "float64"
+}
+
+parse_dates = [
+    "tpep_pickup_datetime",
+    "tpep_dropoff_datetime"
+]
+
+@click.command()
+@click.option('--pg-user', default='root', help='PostgreSQL user')
+@click.option('--pg-pass', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default=5432, type=int, help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
+@click.option('--year', default=2021, type=int, help='Year')
+@click.option('--month', default=1, type=int, help='Month')
+@click.option('--chunksize', default=100000, type=int, help='Chunksize')
+def run(pg_user, pg_pass, pg_host, pg_port, pg_db, target_table, year, month, chunksize):
+
+    prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
+    url = f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
+    df = pd.read_csv(url, dtype=dtype, parse_dates=parse_dates)
+
+    engine = create_engine(f'postgresql+psycopg://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
+
+    df_iter = pd.read_csv(
+        url,
+        dtype=dtype,
+        parse_dates=parse_dates,
+        iterator=True,
+        chunksize=chunksize
+    )
+
+    first=True
+
+    print(
+        f"Creating Table: {target_table},\n"
+        f"Inserting Month: {month:02d},\n"
+        f"Total Records to Process: {len(df)}\n"
+    )
+
+    for df_chunk in tqdm(df_iter):
+        if first:
+            df_chunk.head(0).to_sql(
+                name=target_table,
+                con=engine,
+                if_exists='replace'
+            )
+            first=False
+            print(f"Table Created")
+
+        df_chunk.to_sql(
+            name=target_table,
+            con=engine,
+            if_exists='append'
+        )
+        print(f"Inserted: {len(df_chunk)}")
+
+
+if __name__ == '__main__':
+    run()
---
+++ 01-docker-terraform/pipeline.py
@@ -0,0 +1,31 @@
+import sys
+import pandas as pd
+from pathlib import Path
+
+script_dir = Path(__file__).parent
+output_dir = script_dir/"output"
+output_dir.mkdir(exist_ok=True) # create the directory if it doesn't exist
+
+print("arguments", sys.argv)
+
+day = int(sys.argv[1])
+print(f"Running pipeline for day {day}")
+
+df = pd.DataFrame({
+    "ID": [1,2,3],
+    "name": ["John", "Jane", "Jim"],
+    "day": [2,8,2],
+    })
+
+df = df.loc[df["day"] == day, :]
+
+if df.empty:
+    print(f"No rows with day={day}; exiting.")
+    sys.exit(0)
+
+df.to_parquet(output_dir / f'output_day_{day}.parquet')
+print(df.head())
+print("Pipeline executed successfully")
+
+files = [f.name for f in output_dir.iterdir()]
+print(f'Files in {script_dir}: {files}')
